824a006bf28f7235a97d5ed86c103829a525e711
apsaca <166623651+saca1983@users.noreply.github.com>
2024-12-09 03:32:11 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from '../pages/home/home.component';
+import { LoginComponent } from '../pages/login/login.component';
+import { SectionsComponent } from '../pages/sections/sections.component';
+import { ChallengesComponent } from '../pages/challenges/challenges.component';
+import { ProgressComponent } from '../pages/progress/progress.component';
+import { RewardComponent } from '../pages/reward/reward.component';
+import { AccountComponent } from '../pages/account/account.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, LoginComponent, SectionsComponent, ChallengesComponent, ProgressComponent, RewardComponent, AccountComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../pages/home/home.component';
+import { LoginComponent } from '../pages/login/login.component';
+import { ChallengesComponent } from '../pages/challenges/challenges.component';
+import { ProgressComponent } from '../pages/progress/progress.component';
+import { RewardComponent } from '../pages/reward/reward.component';
+import { SectionsComponent } from '../pages/sections/sections.component';
+import { AccountComponent } from '../pages/account/account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'sections', component: SectionsComponent},
+    {path: 'challenges', component: ChallengesComponent},
+    {path: 'progress', component: ProgressComponent},
+    {path: 'reward', component: RewardComponent},
+    {path: 'account', component: AccountComponent},
+];
